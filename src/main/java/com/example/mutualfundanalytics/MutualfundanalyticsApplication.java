5a8d31cf30b9df5ddce580a356927bae96a3d8de
jayumap <umapjay4757@gmail.com>
2026-03-29 11:55:25 +0530

    initialization

---
+++ src/main/java/com/example/mutualfundanalytics/MutualfundanalyticsApplication.java
@@ -0,0 +1,13 @@
+package com.example.mutualfundanalytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MutualfundanalyticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MutualfundanalyticsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mutualfundanalytics/MutualfundanalyticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mutualfundanalytics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MutualfundanalyticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
